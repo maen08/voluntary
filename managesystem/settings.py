1a421b8f22b33f234a0a0e3eb96e85ca4dbff545
Stanley <2001stany@gmail.com>
2021-01-17 13:44:08 +0300

    staticfiles configured

--- managesystem/settings.py
+++ managesystem/settings.py
@@ -112,5 +112,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'

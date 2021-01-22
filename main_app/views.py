80961eb66400a6f11de7017277189ebd8d314941
Stanley <2001stany@gmail.com>
2021-01-23 01:48:02 +0300

    method to display all user activities added

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render,redirect
 from django.core.mail import EmailMessage
 from django.conf import settings
-from .forms import SystemUserForm
 from .models import SystemUser
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from datetime import timezone
+from .models import SystemActivitie
 
 
 def home_view(request):
@@ -24,6 +25,71 @@ def onbuild_page(request):
 
 
 
+def apply_activity(request):  # will be triggered by the apply
+                
+                                # clicked button by applicant
+    
+    queryset = SystemActivitie.objects.all()
+
+    # check the number of existing/successful applicants
+    # compare to the remaining chance, if true apply if not reject
+
+    
+    return redirect('#') #redirect to his profile
+    
+
+
+
+
+
+
+
+def display_activity(request):
+
+    activity = SystemActivitie.objects.all()
+    args = {
+        'activity':activity
+    }
+
+    return render(request, template_name='activity.html', context=args)
+
+
+
+
+def create_activity(request):
+
+    title = request.POST.get('title')   # display time added
+    description = request.POST.get('description')
+    duration = request.POST.get('duration')
+    place = request.POST.get('place')
+    requirement = request.POST.get('requirement')
+    no_people = request.POST.get('no_people')
+    organization = request.POST.get('organization')
+
+    activity = SystemActivitie(
+        activity_name=title,
+        description=description,
+        requirement=requirement,
+        place=place,
+        people_required=no_people,
+        organization=organization,
+        time=timezone.now()       # double check the field
+
+
+    )
+    try:
+        activity.save():
+        messages.success(request, 'New jobs created')
+
+    except:
+        messages.warning(request, 'New jobs not created')
+
+    return render(request, template_name='create_activity.html')  # create this page
+
+
+
+
+
 def register(request):
     if request.method == 'POST':
         first_name = request.POST.get('firstname')
@@ -35,15 +101,24 @@ def register(request):
         password2 = request.POST.get('password2')
 
         if password2 != password:
-            messages.warning(request, 'password must match!')
+            messages.warning(request, 'Passwords must match!')
             return redirect('register')
 
-    
+        if not '@' and '.' in email:
+            messages.warning(request, 'Invalid email!')
+            return redirect('register')
+
+        filter_username = User.objects.filter(username=username)
+        if filter_username:
+            messages.warning(request, 'This username has already taken!')
+            return redirect('register')
+
         sys_user = SystemUser(
             email=email,
             skill=skill,
             first_name=first_name,
             last_name=last_name,
+            user=request.user
             
         )
 
@@ -56,7 +131,7 @@ def register(request):
                 password=password
             )
             new_user.save()
-            
+
             user = authenticate(
                 request,
                 username=username,

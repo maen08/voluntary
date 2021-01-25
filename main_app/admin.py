87d91e7f31294853764ce386a207ec29ee8489ab
Stanley <2001stany@gmail.com>
2021-01-25 21:28:33 +0300

    customize admin page

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 from .models import SystemUser, SystemActivitie
 
 
 admin.site.register(SystemUser)
 admin.site.register(SystemActivitie)
+
+
+admin.site.unregister(Group)
+admin.site.site_header = "VMS Administration"
